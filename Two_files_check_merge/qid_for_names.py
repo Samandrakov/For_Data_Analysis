1e747131163e27328af24327fafabfa331dd8394
Danila <127055128+Samandrakov@users.noreply.github.com>
2024-07-30 20:44:32 +0300

    3

--- Two_files_check_merge/qid_for_names.py
+++ Two_files_check_merge/qid_for_names.py
@@ -1,7 +1,9 @@
 from meta_names import name
 import requests
+import threading
 
-def get_qid(comp_name):
+
+def get_qid(comp_name, timeout=15):
     url = "https://www.wikidata.org/w/api.php"
     params = {
         'action': 'wbsearchentities',
@@ -9,22 +11,44 @@ def get_qid(comp_name):
         'language': 'en',
         'search': comp_name
     }
-    responce = requests.get(url, params=params)
-    data = responce.json()
-    if data['search']:
-        return data['search'][0]['id']
-    else:
+
+    class RequestThread(threading.Thread):
+        def __init__(self):
+            threading.Thread.__init__(self)
+            self.result = None
+
+        def run(self):
+            try:
+                response = requests.get(url, params=params, timeout=timeout)
+                data = response.json()
+                if data['search']:
+                    self.result = data['search'][0]['id']
+                else:
+                    self.result = None
+            except requests.exceptions.RequestException:
+                self.result = None
+
+    request_thread = RequestThread()
+    request_thread.start()
+    request_thread.join(timeout)
+
+    if request_thread.is_alive():
         return None
+    return request_thread.result
+
 
 company_qids = {}
 
-# names = ['google', 'apple', 'amazon']
-
+# name = ['google', 'apple', 'amazon']
+count = 0
 for n in name:
+    count += 1
     qid = get_qid(n)
     if qid:
+        print(f"{count}, {qid}")
         company_qids[n] = qid
     else:
+        print(f"{count}, {qid}")
         company_qids[n] = '0'
 
 print(company_qids)
--- Two_files_check_merge/test.py
+++ Two_files_check_merge/test.py
@@ -1,9 +1,3 @@
-from meta_names import *
+from qids import *
 
-for i in range(10):
-    print(name[i])
-# print(type(name))
-# print(len(name))
-# print(name[0])
-for n in name:
-    print(n)
+print(qids_names)
